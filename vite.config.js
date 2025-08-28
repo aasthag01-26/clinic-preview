376689c4c6c107f13bdc2086a6076014801e521b
Aastha <aasthag01@gmail.com>
2025-08-28 05:40:22 +0530

    fix: add netlify

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',   // default for Vite, but good to make it explicit
+  },
+  publicDir: 'public' // ensures files like _redirects are copied into dist/
 })
